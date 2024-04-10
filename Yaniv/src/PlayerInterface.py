c818ad6f847d455a507659bf0fd7cf692b42774e
Micael <mikal2003.sp@gmail.com>
2024-04-09 23:30:45 -0300

    fixing the interface

--- Yaniv/src/PlayerInterface.py
+++ Yaniv/src/PlayerInterface.py
@@ -7,19 +7,21 @@ class PlayerInterface:
         self.window.title("Yaniv")
         self.window.geometry("800x600")
         self.window.config(bg="darkgreen")
-        self.window.resizable(width=True, height=True)
+        self.window.resizable(width=False, height=False)
         self.create_widgets()
 
     def create_widgets(self):
         self.buttonTake = tk.Button(self.window, text="Take")
         self.buttonDiscard = tk.Button(self.window, text="Discard")
-        self.buttonTake.place(relx=0.4, rely=0.5, relwidth=0.05, relheight=0.05)
-        self.buttonDiscard.place(relx=0.2, rely=0.5, relwidth=0.05, relheight=0.05)
+        self.buttonTake.place(relx=0.6, rely=0.5, relwidth=0.08, relheight=0.08)
+        self.buttonDiscard.place(relx=0.3, rely=0.5, relwidth=0.09, relheight=0.08)
+        
+        cards = ['2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC']
 
-        imagem = tk.PhotoImage(file="/home/bridge/Área de Trabalho/Yaniv/images/discardMount.svg")
-        w = tk.Label(self.window, image=imagem, width=100, height=100)
+        imagem = tk.PhotoImage(file="/home/bridge/Documentos/INE5417/Yaniv/cards/back.png")
+        w = tk.Label(self.window, image=imagem, bd=0, bg="darkgreen")
         w.imagem = imagem
-        w.pack()
+        w.place(relx=0.3, rely=0.3)
 
 def main():
     root = tk.Tk()
