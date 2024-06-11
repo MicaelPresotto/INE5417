a47d2c53755420b6eced00cfa88447bae5180a49
edugdl <eduardogdelazari2005@gmail.com>
2024-06-10 23:25:07 -0300

    Atualiza Table

--- src/Table.py
+++ src/Table.py
@@ -1,57 +1,65 @@
 from Player import Player
 from DiscardDeck import DiscardDeck
 from BuyDeck import BuyDeck
+from GUIImage import GUIImage
 
 class Table:
     def __init__(self):
         self.players = [Player(f"Player {i}") for i in range(4)]
         self.discardDeck = DiscardDeck()
         self.buyDeck = BuyDeck()
+        self.message = ""
+        self.round = 0
+        self.tableStatus = 0
+        self.selectedDeck = None
+        self.playersQueueIndex = 0
+        self.localPlayerId = ""
 
     def identifyTurnPlayer(self) -> Player:
-        ...
+        for player in self.players:
+            if player.getTurn(): return Player
 
     def getStatus(self) -> int:
-        ...
+        return self.status
 
     def setTableStatus(self, status: int):
-        ...
+        self.status = status
 
-    def isSet(self, cards: list) -> bool:
-        value = cards[0].getValue()
-        for card in cards[1:]:
-            if card.getValue() != value: return False
-        return True
+    def isSet(self, cards : list) -> bool:
+        s = set()
+        for card in cards:
+            value = card.getValue()
+            if value != "0": s.add(value)
+        return len(s) == 1
 
     def isSequence(self, cards: list) -> bool:
-        cards.sort(key = lambda c: c.getValue())
-        suit = cards[0].getSuit()
-        for i in range(1, len(cards)):
-            if cards[i].getSuit() != suit or cards[i].getValue() - 1 != cards[i-1].getValue(): return False
-        return True
-
-    def buyDeck(self):
         ...
 
-    def discardDeck(self):
+    def buyCard(self):
         ...
 
-    def receive_withdrawal_notification(self):
+    def discard(self):
         ...
 
-    def start_match(self):
+    def receiveWithdrawalNotification(self):
         ...
 
-    def reset_game(self):
+    def receiveMove(self, a_move):
+        ...
+
+    def resetGame(self):
+        ...
+
+    def startMatch(self):
         ...
     
-    def receive_move(self, a_move):
+    def optYaniv(self) -> bool:
         ...
 
-    def distribute_cards(self):
+    def distributeCards(self):
         ...
 
-    def getGUIImage(self):
+    def getGUIImage(self) -> GUIImage:
         ...
 
     def resetPlayerQueue(self):
@@ -65,19 +73,14 @@ class Table:
             if player.getTurn == False:
                 player.updateTotalPoints(10)
 
-    def applyPenaltyToOtherPlayers(self, playersQueue: list):
-        for player in playersQueue:
-            if player.getTurn() == False: player.updateTotalPoints(10)
-        
     def endRound(self):
         ...
 
-    def verifyEndOfMatch(self, playersQueue: list) -> bool:
+    def verifyEndOfMatch(self) -> bool:
         ...
-    
-    def isSet(self, cards : list) -> bool:
-        s = set()
-        for card in cards:
-            value = card.getValue()
-            if value != "0": s.add(value)
-        return len(s) == 1
+
+    def updatePlayersQueueIndex(self):
+        self.playersQueueIndex = (self.playersQueueIndex + 1) % len(self.players)
+
+    def getLocalPlayerId(self) -> str:
+        return self.localPlayerId
