a99c92044baebe6a279d427cfceb362da90c087b
edugdl <eduardogdelazari2005@gmail.com>
2024-06-10 23:03:16 -0300

    Implementa PlayerInfo

--- src/PlayerInfo.py
+++ src/PlayerInfo.py
@@ -1,5 +1,23 @@
-from dataclasses import dataclass
-
-@dataclass
 class PlayerInfo:
-    ...
+    def __init__(self, playerId: str, numberOfCards: int, points: int):
+        self.playerId = playerId
+        self.numberOfCards = numberOfCards
+        self.points = points
+
+    def getPlayerId(self):
+        return self.playerId
+
+    def setPlayerId(self, playerId):
+        self.playerId = playerId
+
+    def getNumberOfCards(self):
+        return self.numberOfCards
+
+    def setNumberOfCards(self, numberOfCards):
+        self.numberOfCards = numberOfCards
+
+    def getPoints(self):
+        return self.points
+
+    def setPoints(self, points):
+        self.points = points
